1cac494406e8f83fb83c3b11072c0a7f6b1eb780
Mark Poscablo <mark@arrayfire.com>
2019-08-12 11:47:53 -0500

    Include out in CHECK_ARRAYS

--- src/api/unified/image.cpp
+++ src/api/unified/image.cpp
@@ -58,7 +58,7 @@ af_err af_resize(af_array *out, const af_array in, const dim_t odim0,
 af_err af_transform(af_array *out, const af_array in, const af_array transform,
                     const dim_t odim0, const dim_t odim1,
                     const af_interp_type method, const bool inverse) {
-    CHECK_ARRAYS(in, transform);
+    CHECK_ARRAYS(out, in, transform);
     return CALL(out, in, transform, odim0, odim1, method, inverse);
 }
 
